446fd2b4b1ac31e7e38a60a0ac630dab701a8005
euro <phuchit@omise.co>
2018-09-13 14:28:20 +0700

    Init resources
    :hammer: Migrate to AndroidX
    
    :art: Init graph

---
+++ app/src/main/java/network/omisego/omgwallet/MainActivity.kt
@@ -0,0 +1,17 @@
+package network.omisego.omgwallet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host).navigateUp()
+    }
+}
---
+++ app/src/main/java/network/omisego/omgwallet/MainFragment.kt
@@ -0,0 +1,18 @@
+package network.omisego.omgwallet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import network.omisego.omgwallet.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return binding.root
+    }
+}
