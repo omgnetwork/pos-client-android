6c4b2c07c46e45f07f2bf5049850568fb7dbf25b
euro <phuchit@omise.co>
2019-03-26 14:57:36 +0700

    :sparkles: Add `Toast` Assertion

--- app/src/androidTest/java/network/omisego/omgwallet/setup/custom/assertions/ToastAssertion.kt
+++ app/src/androidTest/java/network/omisego/omgwallet/setup/custom/assertions/ToastAssertion.kt
@@ -7,19 +7,19 @@ package network.omisego.omgwallet.setup.custom.assertions
  * Copyright © 2017-2018 OmiseGO. All rights reserved.
  */
 
+import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import network.omisego.omgwallet.setup.base.BaseInstrumentalTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matcher
 
-fun BaseInstrumentalTest.onToast(msg: String) {
+fun BaseInstrumentalTest.onToast(matcher: Matcher<View>) {
     val decorView = rule.activity.window.decorView
     val visibleAssertion = matches(isDisplayed())
     val toastParentMatcher = withDecorView(not(`is`(decorView)))
-    val toastMatcher = withText(msg)
-    onView(toastMatcher).inRoot(toastParentMatcher).check(visibleAssertion)
+    onView(matcher).inRoot(toastParentMatcher).check(visibleAssertion)
 }
